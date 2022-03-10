6f6fc4a43972cfc1f9956705e6ac13bf97acc960
DBKynd <dave@dbkynd.com>
2022-03-10 15:09:49 -0700

    alter secrets method

--- src/getSecrets.js
+++ src/getSecrets.js
@@ -5,14 +5,9 @@ const secretName = process.env.SECRETS_ARN;
 
 const secretsManager = new AWS.SecretsManager({ region });
 
-module.exports = () => {
-  return new Promise((resolve, reject) => {
-    secretsManager.getSecretValue(
-      { SecretId: secretName },
-      function (err, data) {
-        if (err) reject(err);
-        else resolve(JSON.parse(data.SecretString));
-      },
-    );
+module.exports = new Promise((resolve, reject) => {
+  secretsManager.getSecretValue({ SecretId: secretName }, function (err, data) {
+    if (err) reject(err);
+    else resolve(JSON.parse(data.SecretString));
   });
-};
+});
